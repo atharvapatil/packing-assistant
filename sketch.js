2bdb3a14f5735e26c0d6c104d16fc33cab5b8dfa
atharvapatil <atharvaabhaypatil@gmail.com>
2019-09-10 11:42:00 -0400

    final working v1

--- sketch.js
+++ sketch.js
@@ -3,6 +3,8 @@ let myVideo; // Creating a video caputure from the webcam
 let modelStatus; // Header text
 let resultText;
 let canvas;
+let detectedObj;
+let itemOne, itemTwo, itemThree;
 
 // Loading the MobileNet model before doing anything else
 function preload() {
@@ -26,23 +28,44 @@ function setup() {
   canvas.position((windowWidth - canvas.width) / 2, 100);
 
   myVideo = createCapture(VIDEO);
-  // myVideo.hide();
+  myVideo.hide();
   myVideo.size(canvas.width, canvas.height);
-  myVideo.position((windowWidth - canvas.width) / 2, 200);
+  myVideo.position((windowWidth - canvas.width) / 2, 300);
 
   myMobileNet.classify(myVideo, gotResults);
 
   modelStatus.innerHTML = "Model Loaded";
-}
 
+  itemOne = document.getElementById('item-one');
+  itemOne.classList.add('objectList');
+
+  itemTwo = document.getElementById('item-two');
+  itemTwo.classList.add('objectList');
+
+  itemThree = document.getElementById('item-three');
+  itemThree.classList.add('objectList');
+}
 
 function gotResults(err, results) {
 
-  console.log("The machine is seeing", results);
+  // console.log("The machine is seeing", results);
   resultText.innerHTML = results[0].label;
+
+  if (results) {
+    detectedObj = results[0].label;
+    console.log(detectedObj);
+    if (detectedObj === 'beer glass') {
+      itemOne.classList.add('objectIdentified');
+    } else if (detectedObj === 'sweatshirt') {
+      itemThree.classList.add('objectIdentified');
+    } else if (detectedObj === 'sunglasses, dark glasses, shades') {
+      itemTwo.classList.add('objectIdentified');
+    }
+  }
+
   setTimeout(function() {
     myMobileNet.classify(myVideo, gotResults);
-  }, 2000);
+  }, 1000);
 
 }
 
